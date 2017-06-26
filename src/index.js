61437ee34ade3685f551350ecca0118a81812ae1
Fabien JUIF <fabien.juif@zenika.com>
2017-06-26 16:50:39 +0200

    :sparkles: add multiple index signatures

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import * as actions from './actions'
 import * as selectors from './selectors'
 import reducer from './reducer'
 
-export default middlewares => key => path => prefix => Object.assign(
+const func = middlewares => key => path => prefix => Object.assign(
   reducer(middlewares)(key)(prefix),
   ...Object.keys(actions).map(k => ({ [k]: actions[k](prefix) })),
   ...Object.keys(selectors).map(k => ({ [k]: selectors[k](path)(prefix) })),
 )
+
+export default (...args) => {
+  const length = args.length
+  const [middlewares, key, path, prefix] = args
+
+  if (length < 2) return func(middlewares)
+  if (length === 2) return func(middlewares)(key)
+  if (length === 3) return func(middlewares)(key)(path)
+  return func(middlewares)(key)(path)(prefix)
+}
