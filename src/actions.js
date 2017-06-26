a8d7fc03706f0c1cfa501c916be8c77cb611a35c
Fabien JUIF <fabien.juif@zenika.com>
2017-06-26 15:20:06 +0200

    :hammer: prefix actions (#17)

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,11 @@
-export const SET = prefix => `SET_${prefix}`
+export const SET = prefix => `@trampss/SET_${prefix.toUpperCase()}`
 export const set = prefix => payload => ({ type: SET(prefix), payload })
 
-export const RESET = prefix => `RESET_${prefix}`
+export const RESET = prefix => `@trampss/RESET_${prefix.toUpperCase()}`
 export const reset = prefix => () => ({ type: RESET(prefix) })
 
-export const ADD = prefix => `ADD_${prefix}`
+export const ADD = prefix => `@trampss/ADD_${prefix.toUpperCase()}`
 export const add = prefix => payload => ({ type: ADD(prefix), payload })
 
-export const DEL = prefix => `DEL_${prefix}`
+export const DEL = prefix => `@trampss/DEL_${prefix.toUpperCase()}`
 export const del = prefix => key => ({ type: DEL(prefix), payload: key })
