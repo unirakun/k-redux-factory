60709591dcfd6073c76d913f31777dd883e57170
Fabien JUIF <fabien.juif@zenika.com>
2017-06-26 15:30:06 +0200

    :ok_hand: self review + README

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,13 @@
-export const SET = prefix => `@trampss/SET_${prefix.toUpperCase()}`
+const scope = action => `@trampss/${action.toUpperCase()}`
+
+export const SET = prefix => scope(`SET_${prefix}`)
 export const set = prefix => payload => ({ type: SET(prefix), payload })
 
-export const RESET = prefix => `@trampss/RESET_${prefix.toUpperCase()}`
+export const RESET = prefix => scope(`RESET_${prefix}`)
 export const reset = prefix => () => ({ type: RESET(prefix) })
 
-export const ADD = prefix => `@trampss/ADD_${prefix.toUpperCase()}`
+export const ADD = prefix => scope(`ADD_${prefix}`)
 export const add = prefix => payload => ({ type: ADD(prefix), payload })
 
-export const DEL = prefix => `@trampss/DEL_${prefix.toUpperCase()}`
+export const DEL = prefix => scope(`DEL_${prefix}`)
 export const del = prefix => key => ({ type: DEL(prefix), payload: key })
