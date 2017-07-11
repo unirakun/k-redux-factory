25de1ea530b6bec5f1cae3ffe8de56caf4bb69cb
guigail <guillaume.crespel@gmail.com>
2017-07-11 17:07:21 +0200

    review

--- src/helpers.js
+++ src/helpers.js
@@ -6,13 +6,7 @@ export const mapAction = mapper => () => () => ctx => ({
 export const mapPayload = actionMatches => mapper => () => () => (ctx) => {
   const { payload, type } = ctx.action
   if (!actionMatches || actionMatches.test(type)) {
-    return {
-      ...ctx,
-      action: {
-        ...ctx.action,
-        payload: mapper(payload),
-      },
-    }
+    return mapAction(action => ({ ...action, payload: mapper(payload) }))()()(ctx)
   }
   return ctx
 }
