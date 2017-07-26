2737746fff4bb7475383a4f3a1a9dc63b7af9102
guigail <guillaume.crespel@gmail.com>
2017-07-26 15:12:33 +0200

    :art: reorder structure of types

--- src/types/keyValue.js
+++
@@ -1,7 +0,0 @@
-import keyValue from '../middlewares/keyValue'
-
-export default {
-  middlewares: [keyValue],
-  actions: ['set', 'add', 'reset', 'remove', 'update'],
-  selectors: ['getState', 'getKeys', 'getAsArray', 'getLength', 'isInitialized', 'get', 'getBy'],
-}
--- src/types/simpleObject.js
+++
@@ -1,7 +0,0 @@
-import simpleObject from '../middlewares/simpleObject'
-
-export default {
-  middlewares: [simpleObject],
-  actions: ['set', 'reset', 'update'],
-  selectors: ['getState', 'isInitialized', 'get'],
-}
