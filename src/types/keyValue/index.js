2e0fa9a3d31feb2020873f31700ac25a72a738d4
Fabien JUIF <fabien.juif@zenika.com>
2017-08-01 15:01:09 +0200

    :sparkles: addOrUpdate action (#61)

--- src/types/keyValue/index.js
+++ src/types/keyValue/index.js
@@ -2,6 +2,6 @@ import middleware from './keyValue.middleware'
 
 export default {
   middlewares: [middleware],
-  actions: ['set', 'add', 'reset', 'remove', 'update'],
+  actions: ['set', 'add', 'reset', 'remove', 'update', 'addorupdate'],
   selectors: ['getState', 'getKeys', 'getAsArray', 'getLength', 'isInitialized', 'get', 'getBy'],
 }
--- src/types/keyValue/keyValue.middleware.js
+++ src/types/keyValue/keyValue.middleware.js
@@ -1,10 +1,38 @@
 import { keyBy, without, uniq, omit } from 'lodash'
-import { SET, ADD, UPDATE, REMOVE, RESET } from '../../actions'
+import { SET, ADD, UPDATE, REMOVE, RESET, ADD_OR_UPDATE } from '../../actions'
 
 export const initState = { data: {}, keys: [], array: [], initialized: false }
 
 const keyAlreadyExists =
   state => (key, instanceKey) => state.array.find(o => o[key] === instanceKey)
+
+const add = (key, state, payload) => {
+  let array
+  const instanceKey = payload[key]
+  if (!keyAlreadyExists(state)(key, instanceKey)) {
+    array = [...state.array, payload]
+  } else {
+    array = state.array.map(o => (o[key] === instanceKey ? payload : o))
+  }
+
+  return {
+    ...state,
+    data: { ...state.data, [instanceKey]: payload },
+    keys: uniq([...state.keys, instanceKey]),
+    array,
+    initialized: true,
+  }
+}
+
+const update = (key, state, payload) => {
+  const instanceKey = payload[key]
+
+  return {
+    ...state,
+    data: { ...state.data, [instanceKey]: { ...state.data[instanceKey], ...payload } },
+    array: state.array.map(o => (o[key] === instanceKey ? { ...o, ...payload } : o)),
+  }
+}
 
 const reducer = key => prefix => (/* defaultData */) =>
   (state = initState, { type, payload } = {}) => {
@@ -16,31 +44,14 @@ const reducer = key => prefix => (/* defaultData */) =>
           array: payload,
           initialized: true,
         }
-      case ADD(prefix): {
-        let array
-        const instanceKey = payload[key]
-        if (!keyAlreadyExists(state)(key, instanceKey)) {
-          array = [...state.array, payload]
-        } else {
-          array = state.array.map(o => (o[key] === instanceKey ? payload : o))
-        }
-
-        return {
-          ...state,
-          data: { ...state.data, [instanceKey]: payload },
-          keys: uniq([...state.keys, instanceKey]),
-          array,
-          initialized: true,
-        }
+      case ADD(prefix): return add(key, state, payload)
+      case ADD_OR_UPDATE(prefix): {
+        if (!keyAlreadyExists(state)(key, payload[key])) return add(key, state, payload)
+        return update(key, state, payload)
       }
       case UPDATE(prefix): {
-        const instanceKey = payload[key]
-        if (!keyAlreadyExists(state)(key, instanceKey)) return state
-        return {
-          ...state,
-          data: { ...state.data, [instanceKey]: { ...state.data[instanceKey], ...payload } },
-          array: state.array.map(o => (o[key] === instanceKey ? { ...o, ...payload } : o)),
-        }
+        if (!keyAlreadyExists(state)(key, payload[key])) return state
+        return update(key, state, payload)
       }
       case REMOVE(prefix):
         return {
