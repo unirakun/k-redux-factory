98fbe354c1701b72485ecbb6af275228ff00e5c3
guillaumecrespel <guillaume.crespel@gmail.com>
2017-08-02 14:31:02 +0200

    :sparkles: Helpers reduce and mapState

--- src/helpers.spec.js
+++ src/helpers.spec.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { mapAction, mapPayload } from './helpers'
+import { reducer, mapAction, mapState, mapPayload } from './helpers'
 
 const ctx = {
   action: {
@@ -9,10 +9,31 @@ const ctx = {
   state: {},
 }
 
+const customCtx = {
+  action: {
+    payload: 'payload',
+    type: 'CUSTOM',
+  },
+  state: 'STATE',
+}
+
 const mapperAction = () => 'ACTION MAPPED !'
 const mapperPayload = () => 'PAYLOAD MAPPED !'
+const mapperState = state => `${state} MAPPED !`
+const customReducer = (action, state) => (/CUSTOM/.test(action.type) ? 'CUSTOM_STATE' : state)
 
 const execHelper = context => helper => helper()()()(context)
+
+describe('helpers/reducer', () => {
+  it('should add reducer but not change state', () => expect(execHelper(ctx)(reducer(customReducer))).toMatchSnapshot())
+  it('should add reducer to change state', () => expect(execHelper(customCtx)(reducer(customReducer))).toMatchSnapshot())
+})
+
+describe('helpers/mapState', () => {
+  it('should map state without matching type', () => expect(execHelper(customCtx)(mapState()(mapperState))).toMatchSnapshot())
+  it('should map state (type matcher is KO)', () => expect(execHelper(customCtx)(mapState(/NOTHING/)(mapperState))).toMatchSnapshot())
+  it('should map state (type matcher OK)', () => expect(execHelper(customCtx)(mapState(/CUSTOM/)(mapperState))).toMatchSnapshot())
+})
 
 describe('helpers/mapPayload', () => {
   it('should map payload without matching type', () => expect(execHelper(ctx)(mapPayload()(mapperPayload))).toMatchSnapshot())
