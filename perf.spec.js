a8e1a3ca702a15233b209bc01ddf4f56a653d460
Fabien JUIF <fabien.juif@gmail.com>
2018-07-08 12:52:37 +0200

    :art: better perf print

--- perf.spec.js
+++ perf.spec.js
@@ -11,6 +11,7 @@ const { keyValue } = require('.')
 
 const RERUN = 100
 const RERUN_SELECTORS = 100000
+const LENGTH = 1000
 
 const rerun = callback => Array.from({ length: RERUN }).reduce(callback)
 const rerunSelectors = callback => Array.from({ length: RERUN_SELECTORS }).reduce(callback)
@@ -23,13 +24,17 @@ describe('performance', () => {
   })
 
   const data = {
-    defaultData: Array.from({ length: 1000 }).map(genData('default', 0)),
-    set: Array.from({ length: 10000 }).map(genData('set', 0)),
-    add: Array.from({ length: 1000 }).map(genData('add', 100000)),
-    update: Array.from({ length: 1000 }).map(genData('update', 100000)),
-    remove: Array.from({ length: 1000 }).map(genData('remove', 2)),
-    addOrUpdate: Array.from({ length: 1000 }).map(genData('addOrUpdate', 100500)),
-    getByIds: Array.from({ length: 1000 }).map((...args) => genData('getByIds', 120)(args).code),
+    defaultData: Array.from({ length: LENGTH }).map(genData('default', 0)),
+    sets: Array.from({ length: LENGTH }).map(genData('sets', 0)),
+    add: genData('add', 100000)(0),
+    adds: Array.from({ length: LENGTH }).map(genData('adds', 100002)),
+    update: genData('update', 100000)(0),
+    updates: Array.from({ length: LENGTH }).map(genData('updates', 100002)),
+    remove: genData('remove', 2)(0),
+    removes: Array.from({ length: LENGTH }).map(genData('removes', 4)),
+    addOrUpdate: genData('addOrUpdate', 100500)(0),
+    addOrUpdates: Array.from({ length: LENGTH }).map(genData('addOrUpdates', 100502)),
+    getByIds: Array.from({ length: LENGTH }).map((...args) => genData('getByIds', 120)(args).code),
   }
 
   const timers = {
@@ -37,84 +42,80 @@ describe('performance', () => {
     reads: {},
   }
 
+  const getReadOrWrite = (what) => {
+    switch (what) {
+      case 'sets':
+      case 'add':
+      case 'adds':
+      case 'update':
+      case 'updates':
+      case 'remove':
+      case 'removes':
+      case 'addOrUpdate':
+      case 'addOrUpdates':
+        return 'writes'
+      default: return 'reads'
+    }
+  }
+
+  const runWithTimers = (state, reducer, what, args) => {
+    let result
+    const readOrWrite = getReadOrWrite(what)
+    const multiple = what.endsWith('s')
+    const action = multiple ? what.slice(0, -1) : what
+
+    timers[readOrWrite][what] = Date.now()
+
+    if (readOrWrite === 'writes') result = rerun(() => reducer(state, reducer[action](data[what])))
+    else result = args !== undefined ? rerunSelectors(() => reducer[action === 'getById' ? 'get' : what](...args)(state)) : rerunSelectors(() => reducer[what](state))
+
+    timers[readOrWrite][what] = Date.now() - timers[readOrWrite][what]
+    timers[readOrWrite][what] = `${timers[readOrWrite][what] / RERUN}ms / op [${multiple ? LENGTH : 1}]`
+
+    return result
+  }
+
   it('should print perf', () => {
-    // TODO: make timers results in term of operation per second (OPS)
-    // TODO: test returns
     let state
     timers.all = Date.now()
-    timers.writes.total = Date.now()
 
     // writes
-    timers.writes.init = Date.now()
+    timers.writes.total = Date.now()
     // - init
-    // TODO: should work
+    timers.writes.init = Date.now()
     const reducer = rerun(() => keyValue({ name: 'perf', key: 'code', defaultData: data.defaultData }))
     state = reducer()
     timers.writes.init = Date.now() - timers.writes.init
-    // - set
-    timers.writes.set = Date.now()
-    state = rerun(() => reducer(state, reducer.set(data.set)))
-    timers.writes.set = Date.now() - timers.writes.set
-    // - add
-    timers.writes.add = Date.now()
-    state = rerun(() => reducer(state, reducer.add(data.add)))
-    timers.writes.add = Date.now() - timers.writes.add
-    // - update
-    timers.writes.update = Date.now()
-    state = rerun(() => reducer(state, reducer.update(data.update)))
-    timers.writes.update = Date.now() - timers.writes.update
-    // - remove
-    timers.writes.remove = Date.now()
-    state = rerun(() => reducer(state, reducer.remove(data.remove)))
-    timers.writes.remove = Date.now() - timers.writes.remove
-    // - addOrUpdate
-    timers.writes.addOrUpdate = Date.now()
-    state = rerun(() => reducer(state, reducer.addOrUpdate(data.addOrUpdate)))
-    timers.writes.addOrUpdate = Date.now() - timers.writes.addOrUpdate
+    timers.writes.init = `${timers.writes.init / RERUN}ms / op [1000]`
+    // - writes
+    state = runWithTimers(state, reducer, 'sets')
+    state = runWithTimers(state, reducer, 'add')
+    state = runWithTimers(state, reducer, 'adds')
+    state = runWithTimers(state, reducer, 'update')
+    state = runWithTimers(state, reducer, 'updates')
+    state = runWithTimers(state, reducer, 'remove')
+    state = runWithTimers(state, reducer, 'removes')
+    state = runWithTimers(state, reducer, 'addOrUpdate')
+    state = runWithTimers(state, reducer, 'addOrUpdates')
     // end writes tests
     timers.writes.total = Date.now() - timers.writes.total
 
     // reads
     timers.reads.total = Date.now()
     state = { perf: state }
-    // - getState
-    timers.reads.getState = Date.now()
-    rerunSelectors(() => reducer.getState(state))
-    timers.reads.getState = Date.now() - timers.reads.getState
-    // - getKeys
-    timers.reads.getKeys = Date.now()
-    rerunSelectors(() => reducer.getKeys(state))
-    timers.reads.getKeys = Date.now() - timers.reads.getKeys
-    // - getAsArray
-    timers.reads.getAsArray = Date.now()
-    rerunSelectors(() => reducer.getAsArray(state))
-    timers.reads.getAsArray = Date.now() - timers.reads.getAsArray
-    // - getLength
-    timers.reads.getLength = Date.now()
-    rerunSelectors(() => reducer.getLength(state))
-    timers.reads.getLength = Date.now() - timers.reads.getLength
-    // - isInitialized
-    timers.reads.isInitialized = Date.now()
-    rerunSelectors(() => reducer.isInitialized(state))
-    timers.reads.isInitialized = Date.now() - timers.reads.isInitialized
-    // - get()
-    timers.reads.get = Date.now()
-    rerunSelectors(() => reducer.get()(state))
-    timers.reads.get = Date.now() - timers.reads.get
-    // - get([id])
-    timers.reads.getByIds = Date.now()
-    rerunSelectors(() => reducer.get(data.getByIds)(state))
-    timers.reads.getByIds = Date.now() - timers.reads.getByIds
-    // - get(id)
-    timers.reads.getById = Date.now()
-    rerunSelectors(() => reducer.get(1300)(state))
-    timers.reads.getById = Date.now() - timers.reads.getById
+    // - reads
+    runWithTimers(state, reducer, 'getState')
+    runWithTimers(state, reducer, 'getKeys')
+    runWithTimers(state, reducer, 'getAsArray')
+    runWithTimers(state, reducer, 'getLength')
+    runWithTimers(state, reducer, 'isInitialized')
+    runWithTimers(state, reducer, 'get', [])
+    runWithTimers(state, reducer, 'getById', [19])
+    runWithTimers(state, reducer, 'getByIds', [data.getByIds])
+    runWithTimers(state, reducer, 'hasKey', [137])
     // - getBy
     // TODO:
-    // - hasKey
-    timers.reads.hasKey = Date.now()
-    rerunSelectors(() => reducer.get(137)(state))
-    timers.reads.hasKey = Date.now() - timers.reads.hasKey
+
     // end reads tests
     timers.reads.total = Date.now() - timers.reads.total
 
