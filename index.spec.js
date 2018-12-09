79fad9ef3cb8aabde8de25a35c8115cbaa86b068
Fabien JUIF <fabien.juif@gmail.com>
2018-12-09 12:08:23 +0100

    :white_check_mark: built version (#154)

---
+++ index.spec.js
@@ -0,0 +1,6 @@
+import commonTests from './misc/common-tests'
+import * as libES from './dist/index.es'
+import * as libCJS from './dist/index'
+
+commonTests(libES, 'es version')
+commonTests(libCJS, 'cjs version')
